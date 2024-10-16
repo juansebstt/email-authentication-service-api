d80b8ae80a17af208192b7f746fd755920ec563d
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-10-15 19:15:35 -0500

    first commit

---
+++ src/main/java/com/emailauthenticationserviceapi/EmailAuthenticationServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.emailauthenticationserviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailAuthenticationServiceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailAuthenticationServiceApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/emailauthenticationserviceapi/EmailAuthenticationServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emailauthenticationserviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailAuthenticationServiceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
