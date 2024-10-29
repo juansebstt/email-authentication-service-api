ca64f68e19b56de9ffc8f8ad5e04f25f7e519f1e
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-10-28 21:51:20 -0500

    Partially implemented UserModel

--- src/main/java/com/emailauthenticationserviceapi/common/entity/UserModel.java
+++ src/main/java/com/emailauthenticationserviceapi/common/entity/UserModel.java
@@ -1,6 +1,10 @@
 package com.emailauthenticationserviceapi.common.entity;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Getter
@@ -16,5 +20,31 @@ public class UserModel {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long userId;
+
+    @NotBlank(message = "User email cannot be blank")
+    @NotNull(message = "User email is required")
+    @Schema(description = "Receives the email of the user")
+    @Email
+    private String email;
+
+    @NotBlank(message = "User email cannot be blank")
+    @NotNull(message = "User email is required")
+    @Schema(description = "Receives the email of the user")
+    private String password;
+
+    @NotBlank(message = "User email cannot be blank")
+    @NotNull(message = "User email is required")
+    @Schema(description = "Receives the email of the user")
+    private String firstName;
+
+    @NotBlank(message = "User email cannot be blank")
+    @NotNull(message = "User email is required")
+    @Schema(description = "Receives the email of the user")
+    private String lastName;
+
+    @NotBlank(message = "User email cannot be blank")
+    @NotNull(message = "User email is required")
+    @Schema(description = "Receives the email of the user")
+    private String role;
 
 }
