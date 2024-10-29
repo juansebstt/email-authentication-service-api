d6debcac59d879953f94bc71658a83fa53ab780b
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-10-28 21:44:11 -0500

    Defined ApiPathConstants

---
+++ src/main/java/com/emailauthenticationserviceapi/common/constant/ApiPathConstants.java
@@ -0,0 +1,8 @@
+package com.emailauthenticationserviceapi.common.constant;
+
+public class ApiPathConstants {
+
+    public static final String V1_ROUTE = "/v1";
+    public static final String AUTH_ROUTE = "/auth";
+    
+}
