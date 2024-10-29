24079611397fbf640932aa07615e98aedf6ed3f1
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-10-28 22:09:14 -0500

    Added service files

---
+++ src/main/java/com/emailauthenticationserviceapi/service/JwtService.java
@@ -0,0 +1,4 @@
+package com.emailauthenticationserviceapi.service;
+
+public interface JwtService {
+}
---
+++ src/main/java/com/emailauthenticationserviceapi/service/impl/AuthServiceImpl.java
@@ -0,0 +1,7 @@
+package com.emailauthenticationserviceapi.service.impl;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthServiceImpl {
+}
