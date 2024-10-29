98b7e3c2cb020e36c31a9453a4b16f0315d3780d
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-10-29 08:17:28 -0500

    Implemented AuthApi

--- src/main/java/com/emailauthenticationserviceapi/controller/AuthApi.java
+++ src/main/java/com/emailauthenticationserviceapi/controller/AuthApi.java
@@ -1,4 +1,24 @@
 package com.emailauthenticationserviceapi.controller;
 
+import com.emailauthenticationserviceapi.common.constant.ApiPathConstants;
+import com.emailauthenticationserviceapi.common.dto.LoginRequest;
+import com.emailauthenticationserviceapi.common.dto.TokenResponse;
+import com.emailauthenticationserviceapi.common.dto.UserRequest;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping(ApiPathConstants.V1_ROUTE + ApiPathConstants.AUTH_ROUTE)
 public interface AuthApi {
+    @PostMapping(value = "/register")
+    ResponseEntity<TokenResponse> createUser(
+            @RequestBody @Valid UserRequest userRequest
+    );
+
+    @PostMapping(value = "/login")
+    ResponseEntity<TokenResponse> loginUser(
+            @RequestBody @Valid LoginRequest loginRequest
+    );
 }
