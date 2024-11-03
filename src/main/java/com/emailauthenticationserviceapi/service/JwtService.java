950fe06fa3c0108c5d7f85bcbe9ae21713222a83
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-11-02 22:20:29 -0500

    Implemented JwtService Interface

--- src/main/java/com/emailauthenticationserviceapi/service/JwtService.java
+++ src/main/java/com/emailauthenticationserviceapi/service/JwtService.java
@@ -6,8 +6,12 @@ import io.jsonwebtoken.Claims;
 public interface JwtService {
 
     TokenResponse generateToken(Long userId);
+
     Claims getClaims(String token);
+
     boolean isExpired(String token);
+
     Integer extractedUserId(String token);
+
 
 }
