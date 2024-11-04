1e06ce30391a2a6ebd93d830a3e985a14b6726b0
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-11-04 17:06:50 -0500

    Implemented UserRequest

--- src/main/java/com/emailauthenticationserviceapi/common/dto/UserRequest.java
+++ src/main/java/com/emailauthenticationserviceapi/common/dto/UserRequest.java
@@ -1,4 +1,32 @@
 package com.emailauthenticationserviceapi.common.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserRequest {
+
+    @NotBlank
+    @NotNull
+    @Schema
+    private String name;
+
+    @NotBlank
+    @NotNull
+    @Schema
+    private String email;
+
+    @NotBlank
+    @NotNull
+    @Schema
+    private String password;
+    
 }
