8f3ab5d663cfe4b80952ef5c33a75465c3536051
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-11-05 09:16:54 -0500

    Partially implemented JwtServiceImpl

--- src/main/java/com/emailauthenticationserviceapi/service/impl/JwtServiceImpl.java
+++ src/main/java/com/emailauthenticationserviceapi/service/impl/JwtServiceImpl.java
@@ -1,7 +1,18 @@
 package com.emailauthenticationserviceapi.service.impl;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtServiceImpl {
+
+    private final String secretToken;
+
+    public JwtServiceImpl(@Value("${jwt.secret}") String secretToken) {
+
+        this.secretToken = secretToken;
+    }
+
+    
+
 }
