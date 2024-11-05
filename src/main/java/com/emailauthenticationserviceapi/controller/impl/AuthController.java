b4c70933a8faffded5fa302fdfa17b5db7ad11e5
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-11-05 10:27:19 -0500

    Implemented UserDetailsImpl & AuthController

--- src/main/java/com/emailauthenticationserviceapi/controller/impl/AuthController.java
+++ src/main/java/com/emailauthenticationserviceapi/controller/impl/AuthController.java
@@ -1,7 +1,32 @@
 package com.emailauthenticationserviceapi.controller.impl;
 
+import com.emailauthenticationserviceapi.common.dto.LoginRequest;
+import com.emailauthenticationserviceapi.common.dto.TokenResponse;
+import com.emailauthenticationserviceapi.common.dto.UserRequest;
+import com.emailauthenticationserviceapi.controller.AuthApi;
+import com.emailauthenticationserviceapi.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class AuthController {
+public class AuthController implements AuthApi {
+
+    private final AuthService authService;
+
+    @Autowired
+    public AuthController(AuthService authService) {
+
+        this.authService = authService;
+    }
+
+    @Override
+    public ResponseEntity<TokenResponse> createUser(UserRequest userRequest) {
+        return ResponseEntity.ok(authService.createUser(userRequest));
+    }
+
+    @Override
+    public ResponseEntity<TokenResponse> loginUser(LoginRequest loginRequest) {
+        return ResponseEntity.ok(authService.loginUser(loginRequest));
+    }
 }
--- src/main/java/com/emailauthenticationserviceapi/service/impl/UserDetailsImpl.java
+++ src/main/java/com/emailauthenticationserviceapi/service/impl/UserDetailsImpl.java
@@ -1,7 +1,25 @@
 package com.emailauthenticationserviceapi.service.impl;
 
+import com.emailauthenticationserviceapi.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserDetailsImpl {
+public class UserDetailsImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserDetailsImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return (UserDetails) userRepository.findByEmail(username)
+                .orElseThrow(() -> new RuntimeException("UserDetailsService user not found"));
+    }
 }
